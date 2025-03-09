4c0b6c0815cf41f382cd1810dfc693692f7e423d
chothanijaydip <chothanijaydip98@gmail.com>
2025-03-09 23:14:57 +0530

    Update

--- example/android/app/src/main/kotlin/com/example/exmple/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/exmple/MainActivity.kt
@@ -2,5 +2,4 @@ package com.sentientcode.flutter_rating_bar_pulse_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
